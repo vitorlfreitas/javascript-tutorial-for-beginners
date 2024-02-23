04a23c54bd7342d873b50c93e19875bace9972e5
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-23 15:45:48 +0000

    Modifies and add some extra informations to the file

--- Lesson05 - If Statement/Lesson06 - If Statement.js
+++ Lesson05 - If Statement/Lesson06 - If Statement.js
@@ -24,3 +24,32 @@ else // If any of the previous conditionals were true, this code will be execute
         do this instead
 
 */
+
+/*  Notes: Structure of If... Else
+
+    if (condition) {
+        code
+    }
+    else {
+        code
+    }
+
+    However, we can shrink this code, if the instruction is one line
+
+    if (condition) code
+    else if (condition) code
+    else code
+
+    there is no need of brackets with the code is only one instruction, 
+    Look at the next example:
+
+*/
+
+let n = 15;
+let m = 10;
+
+if (n < m) console.log("The number N is less than the number M");
+if (n > m) console.log("The number N is greater than the number M");
+else console.log("They are equal");
+
+// Compare them, they have the same function, but for this one, we only needed 3 lines of code
