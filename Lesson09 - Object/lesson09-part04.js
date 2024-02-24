b09b3eca4b44c68000de5616a655dc26e87f81f0
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-24 14:26:16 +0000

    Adds the new lesson09-part04 file to Lesson09 - Objects folder

---
+++ Lesson09 - Object/lesson09-part04.js
@@ -0,0 +1,33 @@
+// Iterating Our Objects
+
+// Objects are not iterable, which means that we cannot use for of to iterate them. So what should we do?
+
+// Assign my obj
+const person = {
+    name: 'Vitor',
+    age: 45
+}
+
+// Review: Bracket notation 
+console.log(person['name']); // Vitor
+
+// We Have some Options
+
+// Using the for in
+for (let key in person) {
+    console.log("Attribute: " + key + " | Value: " + person[key]);
+}
+// key is equal to the attributes, so we can use the brackets notation to get the value. 
+
+// Using the method of the Object class
+for (let value of Object.keys(person)) {
+    console.log('Attribute: ' + (value) + ' | Value: ' + person[value]);
+}
+
+// We can also use the in to check if the object has a specific attribute
+if ('name' in person) console.log('There is name attribute in the person object');
+else console.log('There is not name attribute in the person object');
+
+if ('gender' in person) console.log('There is gender attribute in the person object');
+else console.log("There is not gender attribute in the person object");
+
