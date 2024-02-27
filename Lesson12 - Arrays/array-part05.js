ec093d2c90f9f11e14768a06b681b11dc80f77c1
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-27 09:40:41 +0000

    Adds array-part06.js file to the Lesson12 - Arrays folder

--- Lesson12 - Arrays/array-part05.js
+++ Lesson12 - Arrays/array-part05.js
@@ -3,15 +3,38 @@
 // Executes a function on each element of the array individually.
 let fruits = ['Apple', 'Banana', 'Orange', 'Strawberry'];
 
+let startTimeForEach = performance.now();
 fruits.forEach(function (fruit, index) {
     console.log('Index ' + index + ': ' + fruit);
 });
-
+let endTimeForEach = performance.now();
 // The same output, but using the traditional for loop
-
-for (let i = 0; i < fruit.length; i++) {
+let startTimeForLoop = performance.now();
+for (let i = 0; i < fruits.length; i++) {
     console.log('Index ' + i + ': ' + fruits[i]);
 }
+let endTimeForLoop = performance.now();
 
-// Comparing the for-loop with .forEach(), we can say that the for loop has more advantages to use than forEach()?
+/* For loop and For Each has some differences that is important to know
 
+** Loop Control:
+    for loop: It provides more control over the iteration process. You can break out of the loop using break, skip the current iteration using continue, and have more flexibility in controlling the loop flow.
+    forEach(): It does not provide direct control flow statements like break or continue. Once started, it will iterate through all elements of the array unless an exception is thrown.
+
+** Return Value:
+    for loop: It does not implicitly return any value. You can manually collect data during the loop if needed.
+    forEach(): It does not return anything. It's mainly used for its side effects, such as updating variables or performing actions on each element of the array.
+
+** Readability and Conciseness:
+    forEach(): It's often considered more readable and concise, especially for simple iteration tasks. It abstracts away the loop control logic, making the code more expressive.
+    for loop: It may be more suitable for complex looping scenarios where fine-grained control is necessary. 
+
+** Time Complexity:
+    Most of the time, for loop can performance better than for each. However, for each is more readable and easier to maintenance. */
+
+// Let's compare the time complexity
+let timeComplexityForEach = endTimeForEach - startTimeForEach;
+let timeComplexityForLoop = endTimeForLoop - startTimeForLoop;
+
+console.log(`For each: ${timeComplexityForEach}ms`);
+console.log(`For Loop: ${timeComplexityForLoop}ms`);
---
+++ Lesson12 - Arrays/array-part06.js
@@ -0,0 +1,45 @@
+// Lesson12 - Arrays - Reduce()
+
+/* The reduce() method in JavaScript is used to reduce (or accumulate) the values of an array into a single value.
+
+accumulator: The resulting accumulated value after each iteration.
+element: The current element being processed in the array.
+index: The index of the current element.
+array: The original array being reduced.
+initialValue: The initial value of the accumulator. It's optional and if not provided, the first element of the array will be used as the initial value.
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce(function (accumulator, element) {
+    return accumulator + element;
+}, 0); // Initial Value
+
+/* It's important to note that you can use the reduce() method to perform a variety of reduction operations, such as finding the maximum value, minimum value, calculating the average, concatenating strings, etc. It all depends on the reduction function you provide as an argument to the reduce() method. Make sure the reduction function is consistent with the type of operation you want to perform.
+*/
+
+
+console.log(sum); // Result: 15
+
+// Using the Reduce method to find the maximum value
+const numbers2 = [5, 12, 8, 21, 3, 15, 9];
+
+const greatestNumber = numbers2.reduce(function (acc, element) {
+    if (element > acc) 
+        return element;
+    return acc;
+}, numbers2[0]); // Initializing using the first element 
+
+console.log("The greatest number is ", greatestNumber);
+
+// Find the Average
+const numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const avgNumber = numbers3.reduce(function (acc, element) {
+    return acc + element;
+
+}, 0) / numbers3.length;
+
+console.log(avgNumber); // Result: 5.5
+
+
