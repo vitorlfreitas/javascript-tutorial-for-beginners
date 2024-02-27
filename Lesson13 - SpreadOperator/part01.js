3346b8a24b676c0ec2b945b1116c5eca7987f468
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-27 13:43:54 +0000

    Adds Lesson13 - SpreadOperator

---
+++ Lesson13 - SpreadOperator/part01.js
@@ -0,0 +1,53 @@
+/*          *** Spread Operator ***
+
+    There are many ways to use the Spread Operator
+    The Spread Operator "..." was introduced in ES6 and it allows an iterable, such as an array or string, to be expanded or spread into elements.
+
+    * You can use the Spread Operator to Copy Arrays: 
+    
+    const arr = [1, 2, 3];
+    const copyArr = [...arr];
+    
+    * You can use to concatenate arrays easily:
+
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+    const concatenatedArray = [...arr1, ...arr2];
+
+    * Passing Arguments to Function:
+
+    function add(a, b, c) {
+        return a + b + c;
+    }
+
+    const numbers = [1, 2, 3];
+    console.log(add(...numbers)); // Output: 6
+
+    * Creating copies of Objects:
+
+    const obj = { animal: 'cat' };
+    const copy = { ...obj };
+    console.log(copy); // Output: { animal: 'cat' }
+
+
+    * Combine Objects:
+
+    const obj1 = { animal: 'cat' };
+    const obj2 = { animal: 'dog' };
+    const copy = { ...obj1, ...obj2 };
+    console.log(copy); 
+    // Output: { animal: 'cat', animal: 'cat' }
+
+    We can combine an array of primitive types with an array of objects, but there are some
+
+    * See the example below *
+
+    You can copy the codes above and paste below to see them running
+
+    */
+
+const arr = [1, 2, 3, 4]
+const obj = { animal: 'dog' };
+const copy = {...arr, ...obj};
+console.log(copy); 
+/* Output: { '0': 1, '1': 2, '2': 3, '3': 4, animal: 'dog' } */
